040f9b5b69c8f651158836c86da33e87bd2bad9a
Prabhnoor singh <130931542+prabhnnorsingh@users.noreply.github.com>
2023-04-21 00:05:47 +0530

    Add files via upload

---
+++ web development/script.js
@@ -0,0 +1,9 @@
+function buy_junk() {
+  document.getElementsByClassName("sub_tit")[0].style.display = "none";
+  console.log("i got clicked");
+}
+
+function sell_junk() {
+  document.getElementsByClassName("sub_tit")[0].style.display = "inline-block";
+}
+console.log(" iam in");
